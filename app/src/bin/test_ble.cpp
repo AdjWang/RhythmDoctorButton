7c9d89eeef784e291ce136ba8229de2ccf34041d
AdjWang <wwang230513@gmail.com>
2026-07-26 18:55:52 +0800

    Cleanup.

--- app/src/bin/test_ble.cpp
+++ app/src/bin/test_ble.cpp
@@ -6,25 +6,11 @@
 #include <zephyr/sys/printk.h>
 
 int main() {
-  const struct device *gpio0 = DEVICE_DT_GET(DT_NODELABEL(gpio0));
-
-  if (device_is_ready(gpio0)) {
-    gpio_pin_configure(gpio0, 2, GPIO_OUTPUT_INACTIVE);
-    for (int i = 0; i < 8; ++i) {
-      gpio_pin_toggle(gpio0, 2);
-      k_sleep(K_MSEC(100));
-    }
-  }
-
   rdb::BleKeyboard keyboard("RDB BLE Test", "TeamIO");
   keyboard.Begin();
   keyboard.SetBatteryLevel(87);
 
   while (true) {
-    if (device_is_ready(gpio0)) {
-      gpio_pin_toggle(gpio0, 2);
-    }
-
     if (keyboard.is_connected()) {
       printk("ble connected, sending space\n");
       keyboard.Press(' ');
